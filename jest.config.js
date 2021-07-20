70a3c0b77aeec6ee9652a8fbac350c7551ffa8aa
= <ericsilvaccp@gmail.com>
2021-07-20 17:34:09 -0300

    feat: config test setup

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-.controller.ts',
+    '!<rootDir>/src/**/*-.module.ts',
+    '!<rootDir>/src/main/*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
---
+++ src/main.spec.ts
@@ -0,0 +1,5 @@
+describe('Test', () => {
+  it('should test', () => {
+    expect(1 + 1).toBe(2)
+  })
+})
