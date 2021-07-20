4729d807e5cd249a9014030d8115a8da72a87de6
= <ericsilvaccp@gmail.com>
2021-07-20 16:09:24 -0300

    feat: config build with babel

---
+++ src/main/server/index.ts
@@ -0,0 +1,15 @@
+import { Logger } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from '../../modules/app/app.module'
+
+async function bootstrap() {
+  const SERVER_PORT = process.env.SERVER_PORT || 3000
+  const app = await NestFactory.create(AppModule)
+  await app
+    .listen(SERVER_PORT)
+    .then(() =>
+      Logger.log(`Server running in port ${SERVER_PORT}`, 'ServerApplication')
+    )
+}
+
+bootstrap()
