d0c1333305142f20ae46e2520a5cb3f361ff4653
= <ericsilvaccp@gmail.com>
2021-07-20 22:43:16 -0300

    fix: path infra

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { UserModule } from '@modules/user/user.module'
-import { DBModule } from 'infra/db/module/db.module'
+import { DBModule } from '@infra/db/module/db.module'
 
 @Module({
   imports: [UserModule, DBModule]
